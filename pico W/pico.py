b51bec26898ce7431e34a67f98fef7e5f8b0f9de
PauloIsep <mrques.paulo@gmail.com>
2023-10-25 00:57:53 +0100

    night

--- pico W/pico.py
+++ pico W/pico.py
@@ -1,14 +1,64 @@
 from machine import Pin, ADC, PWM          # Do modulo machine importa as classes pin ADC e PWM
-import json
+import ujson
+import os
+import network
+import socket
+import machine
 from time import sleep                     # Do modulo time importa a class sleep 
-led=PWM(Pin(13))             #Define Pin GPIO 13 como Output do PWM gerado
+led=PWM(Pin(0))             #Define Pin GPIO 13 como Output do PWM gerado
 
+led = machine.Pin(0, machine.Pin.OUT)
 
 #fre=ADC(27)   #Define o GPIO 27 com entrada
 #ciclo=ADC(28)        #creating potentiometer object
 
-file = open ('parameters.json','r' )
-data = json.load(file)
+wlan = network.WLAN(network.STA_IF)
+wlan.active(True)
+wlan.connect('NOS-DCEF', '7XXAUKKP')
+
+max_wait = 10
+print('Waiting for connection')
+while max_wait > 10:
+    if wlan.status() < 0 or wlan.status() >= 3:
+        break
+    max_wait -= 1    
+    sleep(1)
+status = None
+if wlan.status() != 3:
+    raise RuntimeError('Connections failed')
+else:
+    status = wlan.ifconfig()
+    print('connection to', 'NOS','succesfull established!', sep=' ')
+    print('IP-adress: ' + status[0])
+ipAddress = status[0]
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(('127.0.0.1', 5000))
+s.listen(1)
+print('Waiting for a connection...')
+
+while True:
+    conn, addr = s.accept()
+    print('Connection from', addr)
+
+    # Receive a command from the client
+    data = conn.recv(1024).decode() #1024 
+    print(data)
+    command = ujson.loads(data)
+    print(command)
+
+    # Turn the GPIO pin on or off based on the received command
+    if command['frequencia'] == '50':
+        print('Turning LED on...')
+        led.value(1)
+    elif command['duty_cycle'] == '60':
+        print('Turning LED off...')
+        led.value(0)
+    else:
+        print('Error')
+    conn.close()
+
+
 
 
 '''
